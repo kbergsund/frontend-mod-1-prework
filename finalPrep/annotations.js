bfff224f5a4ffe4d955d2d37cd1470436030c967
Kyra Bergsund <k.bergsund@gmail.com>
2021-07-26 12:26:51 -0700

    Add finalPrep work

--- finalPrep/annotations.js
+++ finalPrep/annotations.js
@@ -2,41 +2,69 @@
 // Use the double // to create a new comment
 
 //Build a Bear
+
+// Declares a function named 'buildABear' with 5 parameters- name, age, fur, clothes and specialPower
 function buildABear(name, age, fur, clothes, specialPower) {
+  // Declares new variable within function that is a string. It interpolates the name parameter so that when the function is called, the name argument will be 'filled into' the string.
   var greeting = `Hey partner! My name is ${name} - will you be my friend?!`;
+  // Declares new variable within function that is an array combining the name and age parameters.
   var demographics = [name, age];
+  // Declares new variable within function that is a string. It concatenates the specialPower parameter so that when the function is called, the specialPower argument will be 'filled into' the string.
   var powerSaying = "Did you know that I can " + specialPower + " ?";
+  // Declares a new object variable within function. The object has six different object properties defined in key-value pairs.
   var builtBear = {
+    // The basicInfo key has the value of the above demographics variable.
     basicInfo: demographics,
+    // The clothes key has the value of the buildABear fur parameter.
     clothes: clothes,
+    // The exterior key has the value of the buildABear clothes parameter.
     exterior: fur,
+    // The cost key as a fixed integer value.
     cost: 49.99,
+    // The sayings key is assigned the value of an array with three elements: the above greeting and powerSaying variables as well as a new, fixed string for saying goodnight.
     sayings: [greeting, powerSaying, "Goodnight my friend!"],
+    // The isCuddly key has a fixed boolean value of true.
     isCuddly: true,
   };
 
+  // The return keyword enables the computer to output the above builtObject variable, aka the 6 property object, as the result of executing this function.
   return builtBear
 }
 
+// Calls the buildABear function for the first time. Elements within parantheses are the function arguments- a string, integer, string, array, and string, respectively. These arguments take the place of the 5 parameters we identified upon declaring the function. They occupy the various places where the parameters were used within the function. For example, the greeting variable will fill the ${name} section with "Fluffy" to have an end value of `Hey partner! My name is Fluffy - will you be my friend?!` The end result of calling the function will be a builtBear object with all parameters updated with the below values.
 buildABear('Fluffy', 4, 'brown', ['pants', 'jorts', 'tanktop'], 'give you nightmares');
+// Calls the buildABear function for a second time with all new arguments that. The builtBear object is returned with all new values.
 buildABear('Sleepy', 2, 'purple', ['pajamas', 'sleeping cap'], 'sleeping in');
 
 
 
 //FizzBuzz
+
+// Declares a new function called 'fizzBuzz' with 3 parameters- num1, num2, and range
 function fizzBuzz(num1, num2, range) {
+  // Starts a for loop. The conditions of this for loop are that (1) the initial counter variable `i` will start at 0. It is 0 the first time the loop runs. (2) the condition in order to keep looping is that the counter variable `i` must be less than or equal to the range parameter that we pass in from the function declaration parantheses. Once `i` equals the range parameter, it will exit the loop. Finally, (3) the third part of the condition is that at the end of each loop iteration, the counter variable `i` increases by one.
   for (var i = 0; i <= range; i++) {
+    // Sets up a conditional statement that checks two conditions. The && sign means that both conditions must be true in order for this first condition to be met. Condition #1 evaluates if the modulo, or remainder, of our counter variable `i` divided by the num1 parameter is strictly equal to 0. Condition #2 evaluates whether the same is true but for the modulo of `i` divided by the num2 parameter. Only if both modulos are 0 does the code in the curly bracket execute at which time the computer also exits the conditional statement and does not evaluate any below conditions. Otherwise, if not met, it moves on to the next condition.
     if (i % num1 === 0 && i % num2 === 0) {
+      // If both above conditions are met, 'fizzbuzz' will log to the console.
       console.log('fizzbuzz');
+    // Sets up a second condition should the above not be met. In this situation, it only checks one of the two above conditions, that the modulo if `i` divided by the num1 parameter is equal to 0. If so, it will execute the code in the curly brackets and then exit the conditional statement.
     } else if (i % num1 === 0) {
+    // If the modulo of `i` divided by the num1 parameter is strictly equal to zero, 'fixx' will log to the console and the computer will exit the conditional statement. If not, this will not execute and the computer will move on to the next condition.
       console.log('fizz');
+    // Sets up a third condition. Evaluates the second of the initial if statement conditions, whether the modulo of `i` divided by num2 is strictly equal to 0. If so, the computer executes the code in the curly brackets and then exits the conditional statement.
     } else if (i % num2 === 0) {
+      // If the above condition is met, 'buzz' is logged to the console.
       console.log('buzz');
+    //  Final catch-all part of the conditional statement. Only if none of the above conditions are met does the computer reach this `else` statement.
     } else {
+      // If the computer reaches this else statement i.e. none of the above conditions are met, the counter variable `i` gets logged to the console. Ensures that no matter what the arguments are when the function is called, the user will get some kind of feedback. Basically, a list of numbers will print out that are interspersed with fizz and buzz based on the above conditions.
       console.log(i);
     }
   }
 }
 
+// Calls the fizzBuzz function with 3 arguments. num1 is 3, num2 is 5, and range is 100. This means the loop will execute 100 times, evaluating each time if (1) both the modulo of 100/3 and 100/5 is 0 or (2) if not both, one or the other is true, or (3) neither are true. These evaluations will determine what it logs to the console.
 fizzBuzz(3, 5, 100);
-fizzbuzz(5, 8, 400);
+// Calls the function a second time with new arguments. num1 is 5, num2 is 8, and range is 400. The loop will execute 400 times with 5 and 8 acting as the parameters that are used in the modulo calcuations and condition evaluations.
+fizzBuzz(5, 8, 400);
--- finalPrep/modZeroHero.js
+++ finalPrep/modZeroHero.js
@@ -1,52 +1,88 @@
 // Challenge - See if you can follow the instructions and complete the exercise in under 30 minutes!
 
 // Declare two variables - heroName AND specialAbility - set to strings
+var heroName = "Kyra";
+var specialAbility = "laughter";
 
 // Declare two variables - greeting AND catchphrase
 //   greeting should be assigned to a string that uses concatenation to include the heroName
 //   catchphrase should be assigned to a string that uses interpolation to include the specialAbility
+var greeting = "Hello " + heroName + "!";
+var catchsphrase = `${specialAbility} is what we're after!`;
 
 // Declare two variables - power AND energy - set to integers
+power = 11;
+energy = 33;
 
 // Declare two variables - fullPower AND fullEnergy
 //   fullPower should multiply your current power by 500
 //   fullEnergy should add 150 to your current energy
+var fullPower = power * 500;
+var faullEnergy = energy * 150;
 
 // Declare two variables - isHuman and identityConcealed - assigned to booleans
-
+var isHuman = false;
+var identityConcealed = true;
 
 // Declare two variables - archEnemies AND sidekicks
 //   archEnemies should be an array of at least 3 different enemy strings
 //   sidekicks should be an array of at least 3 different sidekick strings
+var archEnemies = ["Syndrome", "Randall", "Darla"];
+var sidekicks = ["Samwise Gamgee", "Chewbacca", "Donkey"];
 
 // Print the first sidekick to your console
+console.log(sidekicks[0]);
 
 // Print the last archEnemy to the console
+console.log(archEnemies[2]);
 
 // Write some code to add a new archEnemy to the archEnemies array
+archEnemies.push("Lots-o");
 
 // Print the archEnemies array to console to ensure you added a new archEnemy
+console.log(archEnemies);
 
 // Remove the first sidekick from the sidekicks array
+sidekicks.shift();
 
 // Print the sidekicks array to console to ensure you added a new sidekick
+console.log(sidekicks);
 
 // Create a function called assessSituation that takes three arguments - dangerLevel, saveTheDay, badExcuse
 //   - dangerLevel should be an integer
-//   - saveTheDay should be a string a hero would say once they save the day 
+//   - saveTheDay should be a string a hero would say once they save the day
 //   - badExcuse should be a string a hero would say if they are too afraid of the dangerLevel
+function assessSituation(dangerLevel, saveTheDay, badExcuse) {
+  dangerLevel = dangerLevel;
+  saveTheDay = saveTheDay;
+  badExcuse = badExcuse;
 
-// Your function should include an if/else statement that meets the following criteria
-//   - Danger levels that are above 50 are too scary for your hero. Any danger level that is above 50 should result in printing the badExcuse to the console
-//   - Anything dangerLevel that is between 10 and 50 should result in printing the saveTheDay string to the console
-//   - If the dangerLevel is below 10, it means it is not worth your time and should result in printing the string "Meh. Hard pass." to the console.
+  // Your function should include an if/else statement that meets the following criteria
+  //   - Danger levels that are above 50 are too scary for your hero. Any danger level that is above 50 should result in printing the badExcuse to the console
+  //   - Anything dangerLevel that is between 10 and 50 should result in printing the saveTheDay string to the console
+  //   - If the dangerLevel is below 10, it means it is not worth your time and should result in printing the string "Meh. Hard pass." to the console.
+
+  if (dangerLevel > 50) {
+    console.log(badExcuse);
+  } else if (dangerLevel > 10) {
+    console.log(saveTheDay);
+  } else {
+    console.log("Meh. Hard pass.");
+  }
+}
 
 //Test Cases
 var announcement = 'Never fear, the Courageous Curly Bracket is here!';
-var excuse = 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.';
-// assessSituation(99, announcement, excuse) > Should print - 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.'
-//assessSituation(21, announcement, excuse) > should print - 'Never fear, the Courageous Curly Bracket is here!'
-//assessSituation(3, announcement, excuse) > should print - "Meh. Hard pass."
+var excuse = 'I think I forgot to lock up my 1992 Toyota Corolla. Be right back.';
+
+//assessSituation(99, announcement, excuse);
+// Should print - 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.' ✅
+
+// assessSituation(21, announcement, excuse)
+// Should print - 'Never fear, the Courageous Curly Bracket is here!' ✅
+
+// assessSituation(3, announcement, excuse)
+// Should print - "Meh. Hard pass." ✅
 
 // Declare a new variable - scaryMonster - assigned to an Object with the following key/values
 //   - name (string)
@@ -56,29 +92,80 @@ var excuse = 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.
 //   - luckyNumbers (array)
 //   - address (object with following key/values: number , street , state, zip)
 
+var scaryMonster = {
+  name:"Sully",
+  smell: "Fabric softener",
+  weight: 333,
+  citiesDestroyed: [ "Monstropolis", "San Bernardino", "Okeechobee"],
+  luckyNumbers: [4, 8, 15, 16, 23, 42],
+  address: {
+    number: 667,
+    street: "Roar Ln",
+    state: "Colorado",
+    zip: 80903
+  }
+}
 
 // Create a new class called SuperHero
 // - Your class should have the following DYNAMIC values
-//   - name 
+//   - name
 //   - superpower
-//   - age 
+//   - age
 // - Your class should have the following STATIC values
 //   - archNemesis, assigned to "The Syntax Error"
 //   - powerLevel = 100
-//   - energyLevel = 50 
+//   - energyLevel = 50
 
-// - Create the following class methods
-//   - sayName, should print the hero's name to the console
-//   - maximizeEnergy, should update the energyLevel to 1000
-//   - gainPower, should take an argument of a number and INCREASE the powerLevel by that number
+class SuperHero {
+  constructor(name, superpower, age) {
+    this.name = name;
+    this.supewpower = superpower;
+    this.age = age;
+    this.archNemesis  = "The Syntax Error";
+    this.powerLevel = 100;
+    this.energyLevel = 50
+  }
+
+  // - Create the following class methods
+  //   - sayName, should print the hero's name to the console
+  //   - maximizeEnergy, should update the energyLevel to 1000
+  //   - gainPower, should take an argument of a number and INCREASE the powerLevel by that number
+
+  sayName() {
+    console.log(this.name);
+  }
+
+  maximizeEnergy() {
+    this.energyLevel = 1000;
+  }
+
+  gainPower(increaseBy) {
+    this.powerLevel += increaseBy;
+  }
+}
 
 // - Create 2 instances of your SuperHero class
 
+var dash = new SuperHero("Dash", "speed", 10);
+var violet = new SuperHero("Violet", "invisibility", 17);
+
+console.log(dash, violet)
+
+// Testing the functions
+dash.maximizeEnergy();
+violet.gainPower(300);
+console.log(dash.energyLevel);
+console.log(violet.powerLevel);
 
 // Reflection
 // What parts were most difficult about this exerise?
 
+// I struggled the most with the differences in syntax between objects and classes. Had to keep referencing my notes because they are very similar but different in key ways, primarly surrounding punctuation (comma versus semi-colon between lines, colon or equal sign for assigning properties) but also with where/how to define a function. I will develop muscle memory. One other area that was tricky was figuring out how to QA code that we weren't instructed to log to the console. I wanted to be sure my class functions were working as desired and did a little bit of troubleshooting with what to log.
+
 // What parts felt most comfortable to you?
+
+// I feel comfortable with variables but know I will need to work on boolean logic the deeper we dive into applications of boolean variables. I also feel comfortable thus far with building conditional statements that account for multiple conditions.
 
 // What skills do you need to continue to practice before starting Mod 1?
 
+// I would say that while I feel really comfortable with the concepts of this Pre-Work, I just need to continue practicing all of it so it becomes second nature and I don't have to have my notes open next to me. I'd also like to work with objects and classes more- to get into OOP -as I know we just scratched the surface.
